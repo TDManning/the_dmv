be0502109feefb9c1d5ae04047033f2355966e15
Terra Manning <terradmanning@gmail.com>
2024-07-11 20:41:20 -0600

    test: add passing tests to set up, name and age it blocks

--- spec/registrant_spec.rb
+++ spec/registrant_spec.rb
@@ -1,26 +1,36 @@
 require 'spec_helper'
 
+RSpec.configure do |config|
+  config.formatter = :documentation
+end
+
 RSpec.describe Registrant do
   before(:each) do
-    @registrant = Registrant.new
     @registrant_1 = Registrant.new('Bruce', 18, true)
     @registrant_2 = Registrant.new('Penny', 15)
   end
 
-  describe '#initialize' do
-    it 'exists' do
-      expect(@registrant_1).to be_a(Registrant)
-      expect(@registrant_2).to be_a(Registrant)
-    end
+  it 'exists' do
+    expect(@registrant_1).to be_an_instance_of(Registrant)
+    expect(@registrant_2).to be_an_instance_of(Registrant)
   end
 
-  xit "can have a name, age and permit" do 
-    expect(@registrant_1.name).to be("Bruce")
-    expect(@registrant_1.age).to be(18)
-    expect(@registrant_1.permit).to be(true)
-    end
+  it 'has a name' do 
+    expect(@registrant_1.name).to eq("Bruce")
+    expect(@registrant_2.name).to eq("Penny")
+  end
+
+  it 'has an age' do
+    expect(@registrant_1.age).to eq(18)
+    expect(@registrant_2.age).to eq(15)
+  end
+
+  xit 'has an license data' do
+    expect(@registrant_1.license_data).to eq({:written=>false, :license=>false, :renewed=>false})
+    expect(@registrant_2.license_data).to eq({:written=>false, :license=>false, :renewed=>false})
+  end
 
   xit "does not have a permit by default" do
-    expect(@registrant2.permit).to eq(false)
+    expect(@registrant_2.permit?).to eq(false)
   end
 end
