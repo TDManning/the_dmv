f73dc941fa5911839714de9540d63f16026f3404
Terra Manning <terradmanning@gmail.com>
2024-07-13 19:18:47 -0600

    fix spacing

--- spec/facility_spec.rb
+++ spec/facility_spec.rb
@@ -52,7 +52,7 @@ RSpec.describe Facility do
       expect(@facility.registered_vehicles).to eq([@cruz]) 
       expect(@cruz.plate_type).to eq(:regular)
       expect(@facility.collected_fees).to eq(100) 
-      # expect(@cruz.registration_date).to eq(Date.today.year)
+      expect(@cruz.registration_date).to eq(Date.today)
     end
   end
 
@@ -63,7 +63,7 @@ RSpec.describe Facility do
       @facility.register_vehicle(@camaro)
       expect(@camaro.plate_type).to eq(:antique)
       expect(@facility.collected_fees).to eq(25) 
-      # expect(@camaro.registration_date).to eq(Date.today.year)
+      expect(@camaro.registration_date).to eq(Date.today)
     end
   end
 
@@ -72,37 +72,30 @@ RSpec.describe Facility do
       expect(@bolt).to be_an_instance_of(Vehicle)
       expect(@facility.add_service('Vehicle Registration')).to eq(["Vehicle Registration"])
       @facility.register_vehicle(@bolt)
-      # expect(@camaro.plate_type).to eq(:ev)
-      # expect(@facility.collected_fees).to eq(200) 
-      # expect(@camaro.registration_date).to eq(Date.today.year)
-   end
+      expect(@bolt.plate_type).to eq(:ev)
+      expect(@facility.collected_fees).to eq(200) 
+      expect(@bolt.registration_date).to eq(Date.today)
+    end
   end
 
   describe "#compile data" do 
-    xit 'can compile all registered vehicle data' do 
-      # expect(@facility.add_service('Vehicle Registration')).to eq(["Vehicle Registration"])
+    it 'can compile all registered vehicle data' do 
+      expect(@facility.add_service('Vehicle Registration')).to eq(["Vehicle Registration"])
+      @facility.register_vehicle(@cruz)
+      @facility.register_vehicle(@camaro)
+      @facility.register_vehicle(@bolt)
       expect(@facility.registered_vehicles).to eq([@cruz, @camaro, @bolt])
-      # expect(@facility.collected_fees).to eq(325) 
-   end
+      expect(@facility.collected_fees).to eq(325) 
+    end
   end
 
   describe "#show other facilities" do 
     it 'cannot register vehicles when the facility is not offering the service' do 
       facility_2 = Facility.new({name: 'DMV Northeast Branch', address: '4685 Peoria Street Suite 101 Denver CO 80239', phone: '(720) 865-4600'})
       expect(@facility_2.services).to eq([]) 
-      # expect(@facility_2.register_vehicle(bolt)).to eq(nil) 
+      expect(@facility_2.register_vehicle(@bolt)).to eq(nil) 
       expect(@facility_2.registered_vehicles).to eq([])
       expect(@facility_2.collected_fees).to eq(0)
     end
   end
 end
-
-# Next I will actually register cruz starting at:
-# @facility.register_vehicle(cruz)
-# cruz.registration_date
-# cruz.plate_type
-# facility_1.registered_vehicles -here we are checking that the vehicle is actually registered
-
-# facility_1.collected_fees -test to show hoe much $$ in fees has been collected so far
-
-#Repeat the process for the camaro and bolt
