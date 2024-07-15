c5aac351a779437a313924d3d685364ce7718095
Terra Manning <terradmanning@gmail.com>
2024-07-15 13:00:12 -0600

    fix: minor tweaks to enumerable locations and other minor fixes

---
+++ lib/facility_factory.rb
@@ -0,0 +1,25 @@
+class FacilityFactory
+  attr_reader :created_facility
+
+  def initialize
+    @created_facility = created_facility
+  end
+  
+  def create_co_facility(locations)
+    created_facility = []
+
+    locations.map do |location|
+      specific_location = {
+        address: location[:address], 
+        collected_fees: location[:collected_fees],
+        name: location[:name],
+        phone: location[:phone],
+        registered_vehicles: location[:registered_vehicles],
+        services: location[:services]}
+
+    co_dmv = Facility.new(specific_location)
+    created_facility << co_dmv
+    end
+    created_facility
+  end
+end
