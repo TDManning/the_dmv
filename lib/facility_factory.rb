79dbc02f41364aee96cc5a73a38d78632f6701e3
Terra Manning <terradmanning@gmail.com>
2024-07-15 14:41:53 -0600

    feat: add methods for ny and wy

--- lib/facility_factory.rb
+++ lib/facility_factory.rb
@@ -22,4 +22,40 @@ class FacilityFactory
     end
     created_facility
   end
+
+#   def create_ny_facility(locations)
+#     created_facility = []
+
+#     locations.map do |location|
+#       specific_location = {
+#         address: location[:address], 
+#         collected_fees: location[:collected_fees],
+#         name: location[:name],
+#         phone: location[:phone],
+#         registered_vehicles: location[:registered_vehicles],
+#         services: location[:services]}
+
+#     ny_dmv = Facility.new(specific_location)
+#     created_facility << ny_dmv
+#     end
+#     created_facility
+#   end
+
+# def create_mo_facility(locations)
+#   created_facility = []
+
+#   locations.map do |location|
+#     specific_location = {
+#       address: location[:address], 
+#       collected_fees: location[:collected_fees],
+#       name: location[:name],
+#       phone: location[:phone],
+#       registered_vehicles: location[:registered_vehicles],
+#       services: location[:services]}
+
+#   mo_dmv = Facility.new(specific_location)
+#   created_facility << mo_dmv
+#   end
+#   created_facility
+# end
 end
